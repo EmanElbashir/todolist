fb240d75a7a7959d8456c9390f123199f8602d3d
Eman Elbashir <114422580+EmanElbashir@users.noreply.github.com>
2023-05-26 02:05:42 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -5,29 +5,27 @@ let maindiv = document.querySelector(".todo");
 let main = document.querySelector(".main");
 
 
-
-
 //add new div
 
 buttun.addEventListener("click", (eo) => {
   eo.preventDefault();
   if (inputnew.value != "") {
-    let neww = `
-<div class="tasks">
-    <p class="item" draggable="true"> ${inputnew.value}</p>
-    <div>
-  <span class="sp"><i class="fa-solid fa-check"></i></span>
-  <span><i class="fa-solid fa-trash-can"></i></span>
-</div>
+   let neww = `
+<div class="tasks" draggable="true">
+    <p class="item" > ${inputnew.value}</p>
+    <div class="span">
+    <span class="sp"><i class="fa-solid fa-check"></i></span>
+    <span><i class="fa-solid fa-trash-can"></i></span>
+  </div>
     
+
   </div>
 `;
   maindiv.innerHTML += neww;
     inputnew.value = "";
-    localStorage.setItem("tod", maindiv.innerHTML);
+    localStorage.setItem("tod", maindiv.innerHTML );
   }
   drageitem();
-
 });
 
 window.onload = function () {
@@ -39,17 +37,16 @@ window.onload = function () {
 
 // delet div
 
-
-
 maindiv.addEventListener("click", (e) => {
   if (e.target.classList.contains("fa-trash-can")) {
     e.target.parentElement.parentElement.parentElement.remove();
-    localStorage.setItem("tod", maindiv.innerHTML);
   } else if (e.target.classList.contains("fa-check")) {
     e.target.classList.replace("fa-check", "fa-face-grin-hearts");
     e.target.parentElement.parentElement.parentElement.style.backgroundColor =
       "#58d558";
   }
+  localStorage.setItem("tod", maindiv.innerHTML);
+
 
 });
 
@@ -57,84 +54,63 @@ maindiv.addEventListener("click", (e) => {
 let drag = null;
 let boxes = document.querySelectorAll(".box");
 
-let delet= document.querySelector(".delet");
-
+let delet = document.querySelector(".delet");
 
 
 function drageitem() {
-  let items = document.querySelectorAll(".item");
+  // let items = document.querySelectorAll(".item");
+  let task = document.querySelectorAll(".tasks");
 
-  items.forEach((item) => {
-    item.addEventListener("dragstart", function () {
-      drag = item;
-      item.style.opacity = "0.5";
-      delet.style.display = 'block';
-
+  task.forEach((tasks) => {
+    tasks.addEventListener("dragstart", function () {
+      drag = tasks;
+      tasks.style.opacity = "0.5";
+      delet.style.display = "block";
     });
 
-    item.addEventListener("dragend", function () {
+    tasks.addEventListener("dragend", function () {
       drag = null;
-      item.style.opacity = "1";
+      tasks.style.opacity = "1";
     });
 
     boxes.forEach((box) => {
       box.addEventListener("dragover", function (e) {
         e.preventDefault();
-        this.style.backgroundColor = "#a996ba";
+        this.style.border = "1px solid red";
+        this.style.paddingBottom = "100px";
 
       });
 
       box.addEventListener("dragleave", function () {
-        this.style.backgroundColor = "#fff";
-
-        
+        this.style.border = "none";
       });
       box.addEventListener("drop", function () {
-        
         this.append(drag);
-        this.style.backgroundColor = "#fff";
-        localStorage.setItem("tod", maindiv.innerHTML);
-
-
+        this.style.marginTop = " 10px";
+        this.style.border="1px solid blueviolet";
+        localStorage.setItem("tod", maindiv.innerHTML );
       });
-
     });
   });
 }
 
 
-
-
-
-
-
-//delet elements
+  //delet elements
 
 var childElements = boxes[2].children;
 
-boxes[2].onclick = function(e) {
-
-if(e.target.classList.contains("delet")){
-
-  for (var i = childElements.length - 1; i > 0; i--) {
-    boxes[2].removeChild(childElements[i]);
-  }
-
-}
-
-}
-
-
-
-
-
-//delet elemnt tasks
-
-maindiv.addEventListener("dragleave", e=>{
-    if (e.target.classList.contains("item")) {
+boxes[2].onclick = function (e) {
+  if (e.target.classList.contains("delet")) {
+    for (var i = childElements.length - 1; i > 0; i--) {
+      boxes[2].removeChild(childElements[i]);
+      e.target.style.display="none"
+      e.target.parentElement.style.border="none";
       
-      e.target.parentElement.remove();}
+    }
 
 
-})
+
+  }
+};
+
 
